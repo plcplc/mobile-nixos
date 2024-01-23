f2f076d35933b0a2d1e21791860ced8cabf76fbd
Philip Lykke Carlsen <plcplc@gmail.com>
2024-01-23 08:02:46 +0100

    plc-example

---
+++ examples/plc/configuration.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkForce;
+  system_type = config.mobile.system.type;
+
+  defaultUserName = "alice";
+in
+{
+  imports = [
+    ./phosh.nix
+  ];
+
+  config = {
+    users.users."${defaultUserName}" = {
+      isNormalUser = true;
+      password = "1234";
+      extraGroups = [
+        "dialout"
+        "feedbackd"
+        "networkmanager"
+        "video"
+        "wheel"
+      ];
+    };
+
+    services.xserver.desktopManager.phosh = {
+      user = defaultUserName;
+    };
+  };
+}
